46f7bdda327de506c6294ec0cdc6d3526c892425
Nikita <berezinsky.nikita@gmail.com>
2019-11-23 07:14:02 +0300

    Recycler view list items

---
+++ app/src/main/java/com/androidacademy/fixit/core/presentation/GetUserAddress.kt
@@ -0,0 +1,16 @@
+package com.androidacademy.fixit.core.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+
+class GetUserAddress(street: String,
+                     house: Int,
+                     building: Int,
+                     appartment: Int,
+                     floor: Int,
+                     porch: Int) {
+
+}
+
+
